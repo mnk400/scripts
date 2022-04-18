21acb21a3e6438f21996afbf1da47bba47ff1313
mnk400 <mnk_400@icloud.comn>
2022-04-17 20:53:42 -0400

    Improvements to updatedots

--- updatedots/updatedots.sh
+++ updatedots/updatedots.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#Script to auto update all the tracked files in my dot file repository
 
+# Script to auto update all the tracked files in my dot file repository
 source echocolors.sh
-#Checking for a commit message
+
+# Checking for a commit message
 if [ "$#" -gt 1 ]; then
     echoWarn "Illegal number of parameters"
     exit
@@ -14,18 +15,19 @@ elif [ "$#" == 1 ]; then
     msg="$1"
 fi
 
-#Creating a commit message
+# Creating a commit message
 commitMsg="$msg: $(date)"
 
-#Adding all the tracked files
+# Adding all the tracked files
 yadm add -u
 
-#Committing
+# Committing
 yadm commit -m "$commitMsg"
-
-#Pushing to github
-yadm push
-
 if [ $? -eq 0 ]; then
+    # Pushing to github
+    yadm push
     echoSuccess "Pushed to Github"
+else
+    echoError "No changes to push"
 fi
+
